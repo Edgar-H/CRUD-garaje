d852d0c6ecb82e3935bee2a1ba50ae7b788883cb
Edgar-H <edgar.hernandezvh@gmail.com>
2020-12-21 15:10:22 -0600

    estetica pc listo

---
+++ js/app.js
@@ -0,0 +1,59 @@
+  // Firebase
+// Your web app's Firebase configuration
+    var firebaseConfig = {
+      apiKey: "AIzaSyD5tlyQP-Ea_GR0r0HbXMfQPswD8Fu10vY",
+      authDomain: "crud-coches.firebaseapp.com",
+      projectId: "crud-coches",
+      storageBucket: "crud-coches.appspot.com",
+      messagingSenderId: "194057359056",
+      appId: "1:194057359056:web:26671bbf7f1b0ed0bd58d5"
+    };
+    // Initialize Firebase
+    firebase.initializeApp(firebaseConfig);
+    // Firebase
+
+
+
+    // Start Add Image Garaje // 
+const imagePreview = document.getElementById('img-preview');
+const imageUploader = document.getElementById('img-uploader');
+const imgUploaderProgress = document.getElementById('img-progress');
+
+const CLOUDINARY_URL = `https://api.cloudinary.com/v1_1/dyykacsdw/image/upload`
+const CLOUDINARY_UPLOAD_PRESET = 'bzcgvodf';
+
+imageUploader.addEventListener('change', async (e) => {
+    const file = e.target.files[0];
+    const formData = new FormData();
+    formData.append('file', file);
+    formData.append('upload_preset', CLOUDINARY_UPLOAD_PRESET);
+
+    // Send to cloudianry
+    const res = await axios.post(
+        CLOUDINARY_URL,
+        formData,
+        {
+            headers: {
+                'Content-Type': 'multipart/form-data'
+            },
+            onUploadProgress (e) {
+                let progress = Math.round((e.loaded * 100.0) / e.total);
+                /* console.log(progress); */
+                imgUploaderProgress.setAttribute('value', progress);
+            }
+        }
+    );
+    /* console.log(res); */
+    imagePreview.src = res.data.secure_url;
+});
+    // End Add Image Garaje //
+
+
+
+
+
+
+
+    // Start CRUD //
+
+    // End CRUD //
