a08cd7f340b37d25e32c0984f560018f71d8be0a
Edgar-H <edgar.hernandezvh@gmail.com>
2020-12-23 22:25:23 -0600

    Me falta capturar la URL de la imagen

--- js/app.js
+++ js/app.js
@@ -50,5 +50,102 @@ imageUploader.addEventListener('change', async (e) => {
 
 
     // Start CRUD //
+const db = firebase.firestore();
+const stockForm = document.getElementById('stock-form');
+const stockContainer = document.getElementById('stock-container');
 
+let editStatus = false;
+let id = '';
+
+const saveCar = (model, brand, colour, year, price) =>
+  db.collection('garaje').doc().set({
+    model,
+    brand,
+    colour,
+    year,
+    price
+  });
+
+const getStock = () => db.collection('garaje').get();
+const onGetStock = (callback) => db.collection('garaje').onSnapshot(callback);
+
+const deleteStock = (id) => db.collection("garaje").doc(id).delete();
+const getEdit = (id) => db.collection("garaje").doc(id).get();
+const updateCar = (id, updatedCar) => db.collection('garaje').doc(id).update(updatedCar);
+
+window.addEventListener('DOMContentLoaded', async (e) => {
+  onGetStock((querySnapshot) => {
+    stockContainer.innerHTML = ''
+    querySnapshot.forEach(doc => {
+
+    const shorck = doc.data();
+    shorck.id = doc.id;
+
+    stockContainer.innerHTML += `<div class="col-lg-3 col-md-5 col-s-12">
+          <h3>${shorck.model} ${shorck.brand}</h3>
+          <p>${shorck.year} ${shorck.colour}</p>
+          <span>$ ${shorck.price}</span>
+          <div>
+            <button class="btn-delete" data-id="${shorck.id}">
+              🗑 Delete
+            </button>
+            <button class="btn-edit" data-id="${shorck.id}">
+              🖉 Edit
+            </button>
+          </div>
+        </div>`;
+
+        const btnsDelete = document.querySelectorAll('.btn-delete');
+        btnsDelete.forEach(btn => {
+          btn.addEventListener('click', async (e) => {
+            await deleteStock(e.target.dataset.id)
+          })
+        });
+
+        const btnsEdit = document.querySelectorAll('.btn-edit')
+        btnsEdit.forEach((btn) => {
+          btn.addEventListener("click", async (e) => {
+            const doc = await getEdit(e.target.dataset.id);
+
+            editStatus = true;
+            id = doc.id;
+
+            stockForm['stock-model'].value = doc.data().model;
+            stockForm['stock-brand'].value = doc.data().brand;
+            stockForm['stock-colour'].value = doc.data().colour;
+            stockForm['stock-year'].value = doc.data().year;
+            stockForm['stock-price'].value = doc.data().price;
+            /* stockForm['btn-addstock-form'].innerText = 'Guardar cambios'; */  //Deshabilito esto porque no me funciona y ya le di varias vueltas
+          });
+      });
+    });
+  });
+});
+
+
+stockForm.addEventListener('submit', async e => {
+  e.preventDefault();
+
+  const model = stockForm['stock-model'];
+  const brand = stockForm['stock-brand'];
+  const colour = stockForm['stock-colour'];
+  const year = stockForm['stock-year'];
+  const price = stockForm['stock-price'];
+
+  if (!editStatus) {
+    await saveCar(model.value, brand.value, colour.value, year.value, price.value);
+  } else {
+    await updateCar(id, {
+      model: model.value, 
+      brand: brand.value, 
+      colour: colour.value, 
+      year: year.value, 
+      price: price.value
+    });
+    editStatus = false;
+    id = ''
+  }
+  stockForm.reset();
+  getStock();
+});
     // End CRUD //
